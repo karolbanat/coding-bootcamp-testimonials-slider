32585c50d63f57e4c8e25e7245ae14dfb1aa603a
Karol Banat <karol.banat@gmail.com>
2022-11-11 18:18:49 +0100

    Changing slides with arrow keys

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
+const KEY_CODES = {
+	arrowLeft: 37,
+	arrowUp: 38,
+	arrowRight: 39,
+	arrowDown: 40,
+};
+
 class Carousel {
 	constructor() {
 		this.carousel = document.querySelector('.carousel');
@@ -6,17 +13,37 @@ class Carousel {
 		this.nextSlideBtn = this.carousel.querySelector('#next-button');
 		this.currentSlideIdx = 0;
 
+		/* binding */
 		this.init = this.init.bind(this);
 		this.showNextSlide = this.showNextSlide.bind(this);
 		this.showPreviousSlide = this.showPreviousSlide.bind(this);
 		this.increaseCurrentSlideIndex = this.increaseCurrentSlideIndex.bind(this);
 		this.decreaseCurrentSlideIndex = this.decreaseCurrentSlideIndex.bind(this);
+		this.handleButtonKeyDown = this.handleButtonKeyDown.bind(this);
 	}
 
 	init() {
 		this.slides[this.currentSlideIdx].classList.add('active');
 		this.prevSlideBtn.addEventListener('click', this.showPreviousSlide);
+		this.prevSlideBtn.addEventListener('keydown', this.handleButtonKeyDown);
 		this.nextSlideBtn.addEventListener('click', this.showNextSlide);
+		this.nextSlideBtn.addEventListener('keydown', this.handleButtonKeyDown);
+	}
+
+	handleButtonKeyDown(e) {
+		if (!e) return;
+		switch (e.keyCode) {
+			case KEY_CODES.arrowLeft:
+			case KEY_CODES.arrowDown:
+				this.showPreviousSlide();
+				this.prevSlideBtn.focus();
+				break;
+			case KEY_CODES.arrowRight:
+			case KEY_CODES.arrowUp:
+				this.showNextSlide();
+				this.nextSlideBtn.focus();
+				break;
+		}
 	}
 
 	showNextSlide() {
